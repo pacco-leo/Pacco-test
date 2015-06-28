39008a4eca0750c0b6fb38a7f0d86690b3731226
matt <matt@trythis.com>
2015-06-28 15:35:04 +0200

    update settings

--- app/paccoProject/settings.py
+++ app/paccoProject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-STATIC_ROOT = "/var/www/example.com/static/"
+STATIC_ROOT = "/var/www/paccotest/static/"
 
 
 # Quick-start development settings - unsuitable for production
